af4b0ea408ad69aafb135d8e6a312692021e6017
Rouz Majlessi <rmajlessi@gmail.com>
2016-10-11 15:21:50 -0700

    add api key

---
+++ js/weather-interface.js
@@ -0,0 +1,14 @@
+var apiKey = "7113fef7e40d9784db42a8d1da81e0d4"
+
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response){
+      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+    }).fail(function(error) {
+      $('.showWeather').text(error.responseJSON.message);
+    });
+  });
+});
